07107716903adbb8660ad939ddeee69e9b0e2ecb
XPH0904 <50739818+XPH0904@users.noreply.github.com>
2020-07-23 23:01:49 +0800

    master

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.frame.MainFrame import *
 #from src.sqlTools.LibrarianTools import *
-#from src.model.Reader import *
+from src.model.Reader import *
 from src.model.Librarian import *
 from src.sqlTools.ReaderTools import *
     
@@ -9,5 +9,16 @@ from src.sqlTools.ReaderTools import *
 #login = LibrarianTools()
 #print(login.LibrarianLogin("root","root"))
 
+person1 = Reader()
+person1.idReader = "011"
+person1.nameReader = "baba"
+person1.level = "教师"
+person1.sex = "男"
+person1.password = "root"
+
+print(person1.list_return_reader())
+
 get_data = ReaderTools()
+
+print(get_data.DeleteReader(person1.idReader))
 print(get_data.ReaderData())
--- src/sqlTools/ReaderTools.py
+++ src/sqlTools/ReaderTools.py
@@ -94,7 +94,96 @@ class ReaderTools:
         except Exception as e:
             traceback.print_exc()
         return ls
-                
+
+    def ReaderLogin(self, idReader , password):
+        db = DatabaseTools()
+        conn = db.getConn()
+        try:
+            sql = "select idReader,password from reader where idReader= '%s' and password= '%s' "
+            answer = (str(idReader),str(password))
+
+            mycursor = conn.cursor()
+
+            mycursor.execute(sql,answer)
+
+            result = mycursor.fetchone()
+
+            if(result != None):
+                return True
+
+            mycursor.close()
+            conn.close()
+        except Exception as e:
+            traceback.print_exc()
+        return False
+
+    def addReader(self, Reader):
+        i = 0
+        db = DatabaseTools()
+        conn = db.getConn()
+        try :
+            sql = "insert into reader (idReader,nameReader,kind,sex,password) values (%s,%s,%s,%s,%s)"
+            answer = (str(Reader.idReader),str(Reader.nameReader),str(Reader.level),str(Reader.sex),str(Reader.password))
+
+            mycursor = conn.cursor()
+
+            mycursor.execute(sql,answer)
+
+            i = mycursor.rowcount            
+
+            mycursor.close()
+            conn.commit()
+            conn.close()
+        except Exception as e :
+            traceback.print_exc()
+        return i
+
+    def UpdateReader(self, Reader):
+        i = 0
+        db = DatabaseTools()
+        conn = db.getConn()
+        try :
+            sql = "update reader set idReader=%s,nameReader=%s,kind=%s,sex=%s,password=%s where idReader=%s"
+            answer = (str(Reader.idReader),str(Reader.nameReader),str(Reader.level),str(Reader.sex),str(Reader.password),str(Reader.idReader))
+
+            mycursor = conn.cursor()
+
+            mycursor.execute(sql,answer)
+
+            i = mycursor.rowcount
+
+            mycursor.close()
+            conn.commit()
+            conn.close()
+        except Exception as e :
+            traceback.print_exc()
+        return i
+    
+    def DeleteReader(self, idreader):
+        i = 0
+        db = DatabaseTools()
+        conn = db.getConn()
+        try :
+            sql = "delete from reader where idReader = %s"
+            answer = (str(idreader),)
+
+            mycursor = conn.cursor()
+
+            mycursor.execute(sql,answer)
+
+            i = mycursor.rowcount
+
+            mycursor.close()
+            conn.commit()
+            conn.close()
+        except Exception as e :
+            traceback.print_exc()
+        return i
+
+
+
+
+
 
 
 
