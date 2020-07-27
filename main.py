e00dacfa5b5fdf076ad269a54a4f32dc2873f519
XPH0904 <50739818+XPH0904@users.noreply.github.com>
2020-07-27 21:03:18 +0800

    master

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from src.frame.MainFrame import *
-from src.frame.Gif_ReaderFrame import *
 #from src.sqlTools.LibrarianTools import *
 from src.model.Book import *
 from src.model.Librarian import *
 from src.sqlTools.LibrarianTools import *
     
 #main = MainFrame()
-
-frame = Gif_ReaderFrame()
-frame.Gif_ReaderFrame()
 
 #login = LibrarianTools()
 #print(login.LibrarianLogin("root","root"))
